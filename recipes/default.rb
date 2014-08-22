9186ca095533bef5ab362843fe5ddc29d2787298
John Bellone <jbellone@bloomberg.net>
2014-08-22 15:41:39 -0400

    Fix a few issues found after attempting to provision.
    The main problem here has to do with Cheffish not being able to support
    public keys in a string representation. Even though the API says this is
    true it was failing attempting to execute #to_pem on the String.

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,7 +23,7 @@ include_recipe 'chef-sugar::default'
 require_chef_gem 'cheffish'
 
 local_server = {
-  chef_server_url: 'https://127.0.0.1',
+  chef_server_url: 'https://127.0.0.1:443',
   options: {
     client_name: node['chef_server_populator']['user'],
     signing_key_filename: node['chef_server_populator']['pem']
@@ -31,9 +31,11 @@ local_server = {
 }
 
 node['chef_server_populator']['clients'].each_pair do |name, item|
+  # TODO: Fix when Cheffish can accept public keys that are strings.
+  public_key = OpenSSL::PKey::RSA.new(item[:client_key])
   chef_client name do
     chef_server local_server
-    source_key item[:client_key]
+    source_key public_key
     validator item[:validator]
     admin item[:admin]
   end
@@ -45,9 +47,11 @@ node['chef_server_populator']['users'].each_pair do |name, item|
   # the correct key.
   next if name == node['chef_server_populator']['user']
 
+  # TODO: Fix when Cheffish can accept public keys that are strings.
+  public_key = OpenSSL::PKey::RSA.new(item[:client_key])
   chef_user name do
     chef_server local_server
-    source_key item[:client_key]
+    source_key public_key
     admin item[:admin]
     email item[:email]
   end
